49fb78b72b47dae2b42b63dc3f975cc088056f05
779742620@qq.com <779742620@qq.com>
2016-11-15 09:32:07 +0800

    helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/*
+	File Name: helloworld.c
+	Author:
+	Mail:
+	Created Time: 2016年11月15日 星期二 09时28分56秒
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	printf("Hello World");
+	return 0;
+}
